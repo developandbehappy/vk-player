f2592a77a0720a80d9556ceccb4c44f397eb7343
zidein07 <zidein07@gmail.com>
2016-03-30 13:43:15 +0300

    update player play music :)

--- src/controllers/playerCtrl.js
+++ src/controllers/playerCtrl.js
@@ -1,3 +1,17 @@
-playerApp.controller('HomeCtrl', ['$scope', '$http', function ($scope) {
+playerApp.controller('playerCtrl', function ($scope, $http) {
+  $scope.dataAudio = '';
+  var dataForUrlVk = {
+    req: 'https://api.vk.com/method/',
+    method: ['audio.get'],
+    token: '2b58094b9415c44531ef2f09fd94263abdd705394cc9d4dd9e74334e54999002243188c07533b6a7aa47c'
+  };
 
-}]);
+  var url = dataForUrlVk.req + dataForUrlVk.method[0] + '?access_token=' + dataForUrlVk.token + '&callback=JSON_CALLBACK';
+
+  $http.jsonp(url).success(function (res) {
+    $scope.dataAudio = res.response;
+    //var response = res.response;
+    //$scope.dataAudio = response;
+    //console.log('$scope.dataAudio', $scope.dataAudio);
+  });
+});
--- src/directive/player.directive.js
+++ src/directive/player.directive.js
@@ -1,4 +1,4 @@
-playerApp.directive('player', function () {
+playerApp.directive('player', function ($timeout) {
   return {
     restrict: 'E',
     replace: true,
@@ -7,8 +7,25 @@ playerApp.directive('player', function () {
       props: '='
     },
     link: function (scope) {
-      $(".nano").nanoScroller();
-      $(".nano").nanoScroller({ sliderMaxHeight: 10 });
+      var allAudio = [];
+      scope.playAudio = function (audioItem) {
+        if (!_.isEmpty(allAudio)) {
+          _.each(allAudio, function (item) {
+            item.stop().play;
+          });
+        }
+        var sound = new Howl({
+          urls: [audioItem.url],
+          autoplay: true,
+          volume: 1
+        });
+        allAudio.push(sound);
+
+      };
+      $timeout(function () {
+        $(".nano").nanoScroller();
+        $(".nano").nanoScroller({sliderMaxHeight: 10});
+      }, 100)
     }
   };
 });
